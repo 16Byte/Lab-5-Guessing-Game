67354afda092d02091d4ac75b56b1ba3bed46d0c
Navi <pixeldevs.diego@gmail.com>
2025-10-01 11:29:42 -0600

    Added colors :3

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -10,15 +10,21 @@ void GuessingGame::StartGame()
     int userGuess = 0;
     int numberOfTries = 0;
 
+    cout << endl;
     Log("Welcome to the Guessing Game!");
     Log("I'm thinking of a number between 1 and 100.");
     Log("Can you guess what it is?");
+    cout << endl;
 
     while(userGuess != numberToGuess && numberOfTries <= 10)
     {
         Log("Enter your guess: ", false);
         cin >> userGuess;
         numberOfTries++;
+        
+        cout << endl;
+        Log("-------------------------------");
+        cout << endl;
 
         if(userGuess == -1)
         {
@@ -33,16 +39,30 @@ void GuessingGame::StartGame()
 
             Log("Seed set! Let's start over. ");
         }
-        else if(userGuess != numberToGuess)
-        {
-            // Wrong guess
-            GiveTheUserAHint(userGuess, numberToGuess);
-        }
         else if(userGuess == numberToGuess)
         {
             // Win condition
             Log("Congratulations! You've guessed the number " + to_string(numberToGuess) + " in " + to_string(numberOfTries) + " tries!");
         }
+        else if(numberOfTries == 10)
+        {
+            // Lose condition
+            Log("Sorry, you've used all your tries. The number was " + to_string(numberToGuess) + ".");
+        }
+
+        // Give a hint every 2 tries
+        if(numberOfTries % 2 == 0 && userGuess != numberToGuess)
+        {
+            cout << BGCYAN << CYAN << "[HINT] " << RESET;
+            GiveTheUserAHint(userGuess, numberToGuess);
+        }
+
+        Log(numberOfTries < 10 ? "You have " + to_string(10 - numberOfTries) + " tries left." : "No tries left. Game over.");
+
+        //debugging
+        Log(" (Debug: Number to guess is " + to_string(numberToGuess) + ")", false);
+        Log(to_string(numberOfTries));
+        cout << endl;
     }
 }
 
@@ -57,9 +77,14 @@ void GuessingGame::GiveTheUserAHint(int userGuess, int numberToGuess)
     // TODO:
     // here we're going to choose a random number
     // in a switch statement we'll use that number to generate a hint for the player
+    
+    if(numberToGuess == (sqrt(userGuess)))
+    {
+        Log("The number you're looking for is the square root of your last guess.");
+    }
 
     //temp
-    if(userGuess < numberToGuess)
+    else if(userGuess < numberToGuess)
     {
         Log("Too low! Try again.");
     }
--- GuessingGame.hpp
+++ GuessingGame.hpp
@@ -11,4 +11,34 @@ class GuessingGame
     private:
         int GetRandomNumber(int min, int max, unsigned int seed = 0);
         void GiveTheUserAHint(int userGuess, int numberToGuess);
+
+    //the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+    #define RESET   "\033[0m"
+    #define BLACK   "\033[30m"      /* Black */
+    #define RED     "\033[31m"      /* Red */
+    #define GREEN   "\033[32m"      /* Green */
+    #define YELLOW  "\033[33m"      /* Yellow */
+    #define BLUE    "\033[34m"      /* Blue */
+    #define MAGENTA "\033[35m"      /* Magenta */
+    #define CYAN    "\033[36m"      /* Cyan */
+    #define WHITE   "\033[37m"      /* White */
+    #define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+    #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+    #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+    #define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+    #define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+    #define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+    #define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+    #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+    // Background color codes (Ubuntu/Linux, macOS only)
+    #define BGBLACK   "\033[40m"      /* Black background */
+    #define BGRED     "\033[41m"      /* Red background */
+    #define BGGREEN   "\033[42m"      /* Green background */
+    #define BGYELLOW  "\033[43m"      /* Yellow background */
+    #define BGBLUE    "\033[44m"      /* Blue background */
+    #define BGMAGENTA "\033[45m"      /* Magenta background */
+    #define BGCYAN    "\033[46m"      /* Cyan background */
+    #define BGWHITE   "\033[47m"      /* White background */
+
 };
