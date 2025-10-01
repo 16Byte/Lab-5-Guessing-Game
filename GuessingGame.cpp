31ca3090267b78ca0f80e1be5bc8f8f80dcdfb7e
Navi <pixeldevs.diego@gmail.com>
2025-09-30 21:57:18 -0600

    Added a try cap and cleaned up the get random number function

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -6,16 +6,15 @@ using namespace std;
 
 void GuessingGame::StartGame()
 {
-    //Log(to_string(GetRandomNumber(1, 100, 542)));
-
-    Log("Welcome to the Guessing Game!");
-    Log("I'm thinking of a number between 1 and 100.");
-    Log("Can you guess what it is?");
     int numberToGuess = GetRandomNumber(1, 100);
     int userGuess = 0;
     int numberOfTries = 0;
 
-    while(userGuess != numberToGuess)
+    Log("Welcome to the Guessing Game!");
+    Log("I'm thinking of a number between 1 and 100.");
+    Log("Can you guess what it is?");
+
+    while(userGuess != numberToGuess && numberOfTries <= 10)
     {
         Log("Enter your guess: ", false);
         cin >> userGuess;
@@ -55,6 +54,11 @@ void GuessingGame::Log(string message, bool newLine)
 
 void GuessingGame::GiveTheUserAHint(int userGuess, int numberToGuess)
 {
+    // TODO:
+    // here we're going to choose a random number
+    // in a switch statement we'll use that number to generate a hint for the player
+
+    //temp
     if(userGuess < numberToGuess)
     {
         Log("Too low! Try again.");
@@ -68,19 +72,18 @@ void GuessingGame::GiveTheUserAHint(int userGuess, int numberToGuess)
 int GuessingGame::GetRandomNumber(int min, int max, unsigned int seed)
 {
     random_device rand;
+    uniform_int_distribution<> random(min, max);
 
     if(seed == 0)
     {
         // Use a random seed
         mt19937 gen(rand());
-        uniform_int_distribution<> random(min, max);
         return random(gen);
     }
     else
     {
         // Use the provided seed
         mt19937 gen(seed);
-        uniform_int_distribution<> random(min, max);
         return random(gen);
     }
     return -404; // Error -- seed not found
